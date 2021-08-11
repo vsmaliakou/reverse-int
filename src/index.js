6c80c79fc634a1cbae26d219bcbf1a6d16cd182b
vsmaliakou <vsmaliakou@yandex.by>
2021-08-11 21:04:48 +0300

    feat: solve the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
 module.exports = function reverse (n) {
-  
+    return n < 0 ? Number(n.toString().split("").splice(1).reverse().join("")) : Number(str = n.toString().split("").reverse().join(""))
 }
